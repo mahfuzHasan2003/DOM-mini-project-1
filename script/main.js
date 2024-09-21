e449c91692ac21504f6a03586e5662fd05085075
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2024-09-21 15:19:06 +0600

    added new functionality

--- script/main.js
+++ script/main.js
@@ -1,12 +1,4 @@
-// const income = document.getElementById("income");
-// const software = document.getElementById("software");
-// const courses = document.getElementById("courses");
-// const internet = document.getElementById("internet");
-// const savings = document.getElementById("savings");
-// const totalExpenses = document.getElementById("total-expenses");
-// const balance = document.getElementById("balance");
-// const savingsAmount = document.getElementById("savings-amount");
-// const remainingBalance = document.getElementById("remaining-balance");
+// input validation
 const allInputs = [
   getElement("income", null),
   getElement("software", null),
@@ -14,15 +6,16 @@ const allInputs = [
   getElement("internet", null),
   getElement("savings", null),
 ];
-// input validation
 allInputs.forEach((inp) => {
   inp.addEventListener("input", () => {
     handleError(inp);
   });
 });
 
-// updating Results Summary
+// addevent to calculate BTN
 getElement("calculate", null).addEventListener("click", () => {
+  // updating result summary
+  getElement("results", null).classList.remove("hidden");
   getElement("total-expenses", null).innerText = getTotalExpenses(
     getElement("software", "val"),
     getElement("courses", "val"),
@@ -32,5 +25,58 @@ getElement("calculate", null).addEventListener("click", () => {
     getElement("income", "val"),
     getElement("total-expenses", "textVal")
   );
-  //   getElementVal("balance", false).innerText =
+
+  // adding expense history
+  getElement("history-list", null).innerHTML =
+    `<div class="bg-white p-3 rounded-md shadow-sm
+      border-l-2 border-blue-500"
+      >
+       <p class="text-xs text-gray-500"> ${new Date().toLocaleDateString()}</p>
+       <p class="text-xs text-gray-500">Income : $${getElement(
+         "income",
+         "val"
+       )}</p>
+       <p class="text-xs text-gray-500"> Expenses : $${getElement(
+         "total-expenses",
+         "textVal"
+       )} </p>
+       <p class="text-xs text-gray-500"> Balance : $${getElement(
+         "balance",
+         "textVal"
+       )} </p>
+  </div>` + getElement("history-list", null).innerHTML;
+});
+
+// tab switching and updating content
+getElement("history-tab", null).addEventListener("click", () => {
+  getElement("expense-form", null).classList.add("hidden");
+  getElement("history-section", null).classList.remove("hidden");
+  getElement("assistant-tab", null).classList.remove(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
+  getElement("history-tab", null).classList.add(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
+});
+getElement("assistant-tab", null).addEventListener("click", () => {
+  getElement("expense-form", null).classList.remove("hidden");
+  getElement("history-section", null).classList.add("hidden");
+  getElement("history-tab", null).classList.remove(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
+  getElement("assistant-tab", null).classList.add(
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600",
+    "text-white"
+  );
 });
